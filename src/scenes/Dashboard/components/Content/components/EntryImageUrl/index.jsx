aaa74e8022087eb75b0ff405b2c5aa7df1cc8f95
samrmay <samrmay@DESKTOP-S2TR4L5.localdomain>
2020-10-06 21:59:37 -0400

    added edit function to imgUrl datatype

--- src/scenes/Dashboard/components/Content/components/EntryImageUrl/index.jsx
+++ src/scenes/Dashboard/components/Content/components/EntryImageUrl/index.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import InteractableText from "../../../../../../components/InteractableText";
+import editIcon from "../../../../../../assets/edit.svg";
 import styles from "./styles.css";
 
 class EntryImageUrl extends React.Component {
@@ -7,17 +9,45 @@ class EntryImageUrl extends React.Component {
     this.state = {
       beingEdited: false,
     };
+    this.handleEditClick = this.handleEditClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleEditClick() {
+    this.setState({ beingEdited: true });
+  }
+
+  handleChange(value) {
+    this.props.handleChange(this.props.index, value);
   }
 
   render() {
-    const { value, name, width, visible } = this.props;
+    const { value, name, width, visible, editable } = this.props;
     let { display } = this.props;
     if (!visible) {
       display = "none";
     }
-    const style = { width, display };
+    const style = { display };
 
-    return <img src={value} alt={name} style={style} />;
+    return (
+      <div style={style}>
+        {editable ? (
+          <div className={styles.editContainer}>
+            <img
+              src={editIcon}
+              className={styles.editIcon}
+              onClick={this.handleEditClick}
+            />
+            <InteractableText
+              value={value}
+              editable={this.state.beingEdited}
+              handleChange={this.handleChange}
+            />
+          </div>
+        ) : null}
+        <img src={value} alt={name} style={{ width: width }} />
+      </div>
+    );
   }
 }
 
